e88b5e0fc5537755d9921bf82c8d4c922d82beb8
Pavel Vasiliev <vas-pav@mail.ru>
2022-12-03 19:44:54 +0300

    Выполнить третий шаг

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        var userName= scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+    }
+}
+
