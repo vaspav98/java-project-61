2765fbb710d5d325948ce41cf03caa816f0d7c4b
Pavel Vasiliev <vas-pav@mail.ru>
2022-12-08 19:04:27 +0300

    Выполнить пятый шаг

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 [![Actions Status](https://github.com/vaspav98/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/vaspav98/java-project-61/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/e234fe07efb6fd2dc36d/maintainability)](https://codeclimate.com/github/vaspav98/java-project-61/maintainability)
+[![asciicast](https://asciinema.org/a/544084.svg)](https://asciinema.org/a/544084)
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,19 @@
 package hexlet.code;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        var answer = scanner.next();
+        if (answer.equals("1")) {
+            Cli.greeting();
+        }
+        if (answer.equals("2")) {
+            Even.game();
+        }
     }
 }
